fcf923231b7ec0679b618df01c7120545a609f27
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-18 01:00:23 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,8 @@
 # Write your solution here
 
-this_year = Time.now.year
-puts "Hey, it's not 2019!" unless this_year == 2019
+current_time = Time.now
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
